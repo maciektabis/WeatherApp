195957a1ba147824b43e2766636bd0e33a1287d6
Maciej Tabiś <maciek.tabis@gmail.com>
2023-01-25 21:15:13 +0100

    Add the enter funcionality and end of the project

--- script.js
+++ script.js
@@ -15,35 +15,48 @@ const getWeather = () => {
 	const city = input.value || 'London';
 	const URL = API_LINK + city + API_KEY + API_UNITS;
 
-	axios.get(URL).then((res) => {
-		console.log(res.data);
-		const temp = res.data.main.temp;
-		const hum = res.data.main.humidity;
-		const status = Object.assign({}, ...res.data.weather);
-		console.log(status);
-		if (status.id >= 200 && status.id < 300) {
-			photo.setAttribute('src', './img/thunderstorm.png');
-		} else if (status.id >= 300 && status.id < 400) {
-			photo.setAttribute('src', './img/drizzle.png');
-		} else if (status.id >= 500 && status.id < 600) {
-			photo.setAttribute('src', './img/rain.png');
-		} else if (status.id >= 600 && status.id < 700) {
-			photo.setAttribute('src', './img/snow.png');
-		} else if (status.id >= 700 && status.id < 800) {
-			photo.setAttribute('src', './img/fog.png');
-		} else if (status.id === 800) {
-			photo.setAttribute('src', './img/sun.png');
-		} else if (status.id >= 800 && status.id < 900) {
-			photo.setAttribute('src', './img/cloud.png');
-		} else {
-			photo.setAttribute('src', './img/unknown.png');
-		}
+	axios
+		.get(URL)
+		.then((res) => {
+			console.log(res.data);
+			const temp = res.data.main.temp;
+			const hum = res.data.main.humidity;
+			const status = Object.assign({}, ...res.data.weather);
+			console.log(status);
 
-		cityName.textContent = res.data.name;
-		weather.textContent = status.main;
-		temperature.textContent = Math.floor(temp) + '℃';
-		humidity.textContent = hum + '%';
-	});
+			cityName.textContent = res.data.name;
+			weather.textContent = status.main;
+			temperature.textContent = Math.floor(temp) + '℃';
+			humidity.textContent = hum + '%';
+			warning.textContent = '';
+			input.value = '';
+
+			if (status.id >= 200 && status.id < 300) {
+				photo.setAttribute('src', './img/thunderstorm.png');
+			} else if (status.id >= 300 && status.id < 400) {
+				photo.setAttribute('src', './img/drizzle.png');
+			} else if (status.id >= 500 && status.id < 600) {
+				photo.setAttribute('src', './img/rain.png');
+			} else if (status.id >= 600 && status.id < 700) {
+				photo.setAttribute('src', './img/snow.png');
+			} else if (status.id >= 700 && status.id < 800) {
+				photo.setAttribute('src', './img/fog.png');
+			} else if (status.id === 800) {
+				photo.setAttribute('src', './img/sun.png');
+			} else if (status.id >= 800 && status.id < 900) {
+				photo.setAttribute('src', './img/cloud.png');
+			} else {
+				photo.setAttribute('src', './img/unknown.png');
+			}
+		})
+		.catch(() => (warning.textContent = 'Wpisz poprawną nazwę miasta!'));
+};
+const enterCheck = (e) => {
+	if (e.key === 'Enter') {
+		getWeather();
+	}
 };
 getWeather();
+
 button.addEventListener('click', getWeather);
+input.addEventListener('keyup', enterCheck);
