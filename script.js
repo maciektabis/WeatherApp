ca8622778a174767e4520496b6306c9ff88f3941
Maciej Tabiś <maciek.tabis@gmail.com>
2023-01-25 20:13:08 +0100

    Assigning id to images

--- script.js
+++ script.js
@@ -16,13 +16,34 @@ const getWeather = () => {
 	const URL = API_LINK + city + API_KEY + API_UNITS;
 
 	axios.get(URL).then((res) => {
+		console.log(res.data);
 		const temp = res.data.main.temp;
 		const hum = res.data.main.humidity;
+		const status = Object.assign({}, ...res.data.weather);
+		console.log(status);
+		if (status.id >= 200 && status.id < 300) {
+			photo.setAttribute('src', './img/thunderstorm.png');
+		} else if (status.id >= 300 && status.id < 400) {
+			photo.setAttribute('src', './img/drizzle.png');
+		} else if (status.id >= 500 && status.id < 600) {
+			photo.setAttribute('src', './img/rain.png');
+		} else if (status.id >= 600 && status.id < 700) {
+			photo.setAttribute('src', './img/snow.png');
+		} else if (status.id >= 700 && status.id < 800) {
+			photo.setAttribute('src', './img/fog.png');
+		} else if (status.id === 800) {
+			photo.setAttribute('src', './img/sun.png');
+		} else if (status.id >= 800 && status.id < 900) {
+			photo.setAttribute('src', './img/cloud.png');
+		} else {
+			photo.setAttribute('src', './img/unknown.png');
+		}
 
 		cityName.textContent = res.data.name;
-
+		weather.textContent = status.main;
 		temperature.textContent = Math.floor(temp) + '℃';
 		humidity.textContent = hum + '%';
 	});
 };
 getWeather();
+button.addEventListener('click', getWeather);
